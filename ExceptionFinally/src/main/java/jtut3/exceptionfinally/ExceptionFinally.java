4265f6baa70fde7feec7c41579c74f58f081558c
Vakhtang Tivadze <vaxotivadze@gmail.com>
2020-10-03 00:07:44 +0400

    Exception finally block example

---
+++ ExceptionFinally/src/main/java/jtut3/exceptionfinally/ExceptionFinally.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jtut3.exceptionfinally;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author DELL
+ */
+public class ExceptionFinally {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String n_1 = JOptionPane.showInputDialog(null, "Enter number 1");
+        String n_2 = JOptionPane.showInputDialog(null, "Enter number 2");
+        
+        try {
+            int a = Integer.parseInt(n_1);
+            int b = Integer.parseInt(n_2);
+            JOptionPane.showMessageDialog(null, "Sum of numbers: " + (a + b));
+        } catch (NumberFormatException e) {
+            System.out.println(e);
+        } finally {
+            System.out.println("Program is ended");
+        }
+    }
+    
+}
